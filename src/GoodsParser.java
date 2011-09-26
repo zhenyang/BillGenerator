1fe8f5d2e98fbbd4954b3cd5c61e6f59db290af9
zhenyang <nwu.scott119@gmail.com>
2011-09-26 22:55:49 +0800

    refectoring GoodsParser

--- src/GoodsParser.java
+++ src/GoodsParser.java
@@ -2,11 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GoodsParser {
+
+    private GoodFactory goodFactory;
+
+    public GoodsParser() {
+        goodFactory = new GoodFactory();
+    }
+
     public List<Good> generateGoods(String shoppingList) {
         List<String> names = parseNames(shoppingList);
         List<Good> goods = new ArrayList<Good>();
         for (String name : names) {
-            goods.add(new GoodFactory().createGood(name));
+            goods.add(goodFactory.createGood(name));
         }
         return goods;
     }
